618556bd7810dbb99503036e651d388c7d3f288b
JohsuaJi <JiXu204@gmail.com>
2015-11-01 01:37:01 -0400

    javadoc by joshua

---
+++ src/CaptureTheFlag/LCDdisplay.java
@@ -0,0 +1,20 @@
+package CaptureTheFlag;
+
+
+import java.util.Timer;
+import lejos.utility.TimerListener;
+
+public class LCDdisplay implements TimerListener {
+
+	private Timer timer;
+	private TextLCD lcd;
+	
+	/**
+	 * Constantly drawing data/information on screen 
+	 */
+	@Override
+	public void timedOut() {
+		
+	}
+
+}
